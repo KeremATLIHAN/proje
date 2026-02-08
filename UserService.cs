f6421bf2047711b87b5aa170541df94d7b0225e9
Esra <esrayildirim41.tr@gmail.com>
2026-02-08 11:55:00 +0300

    Login eklendi

--- UserService.cs
+++ UserService.cs
@@ -2,6 +2,23 @@ namespace ECommerce
 {
     public class UserService
     {
-        
+        public bool Login(string username, string password)
+        {
+            // Basitlik olsun diye hardcoded kontrol yapiyoruz
+            if (username == "admin" && password == "1234")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n[Sistem] Giris basarili! Hosgeldin " + username);
+                Console.ResetColor();
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n[Hata] Kullanici adi veya sifre hatali!");
+                Console.ResetColor();
+                return false;
+            }
+        }
     }
 }
